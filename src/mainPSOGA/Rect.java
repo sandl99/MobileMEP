fa6ae12a45a55c8f578cec91f6a93c759d4826f2
Dang Lam San 20170111 <san.dl170111@sis.hust.edu.vn>
2020-03-01 15:58:54 +0700

    chane

--- src/mainPSOGA/Rect.java
+++ src/mainPSOGA/Rect.java
@@ -16,7 +16,7 @@ public class Rect {
 		PrintWriter pw;
 		String[] str = {"Rect"};
 		for (String s : str) {
-			for (int nums = 1; nums <= 4; nums++) {
+			for (int nums = 2; nums <= 4; nums++) {
 				int n = 25 * nums;
 				for (int i = 21; i <= 21; i++) {
 
